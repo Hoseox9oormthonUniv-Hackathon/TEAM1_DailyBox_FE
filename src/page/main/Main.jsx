6a4bba5069ad5e37f844ccd006e9be6d46848be3
Hina0222 <hina0222@naver.com>
2024-09-28 18:43:17 +0900

    세팅

---
+++ src/page/main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainPage = styled.div`
+    min-height:100vh;
+    background-color: #2C2C2C;
+`;
+
+const Main = () => {
+    return (
+        <MainPage>
+            <h1>가가</h1>
+        </MainPage>
+    );
+};
+
+export default Main;
